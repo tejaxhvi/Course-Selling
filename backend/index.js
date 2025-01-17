fc6c531a3f7481246940d94e7be6cb1fdd2758ce
tejaxhvi <tajeshviyadav@gmail.com>
2025-01-17 20:25:47 +0530

    fixed multiple calling database calls from frontend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongo = require('mongoose')
+const cors = require('cors')
 
 const { PORT } = require('./config')
 
@@ -8,7 +9,23 @@ const { UserRoutes } = require('./Routes/user')
 const { AdminRoutes } = require('./Routes/admin')
 const { CoursesRoutes } = require('./Routes/courses') 
 
+const { CoursesModel } = require('./db')
+
 app.use(express.json())
+app.use(cors())
+
+app.get('/api/courses' , async (req , res) => { // show all the courses available on the site
+    
+    const Courses = await CoursesModel.find({
+        // find everything in database without authentication
+    })
+    console.log(Courses);
+    
+
+    res.json({
+        Courses,
+    })
+})
 
 app.use('/user' , UserRoutes)
 app.use('/admin', AdminRoutes)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [course, setCourse] = useState([])
 
+  useEffect(()=>{
+    axios.get('http://localhost:3001/api/courses')
+    .then((response)=>{
+      setCourse(response.data.Courses)
+      console.log(response.data.Courses);
+      
+    })
+  },[])
   return (
+    
     <>
       <h1>Course-Selling Website</h1>
-      <h2></h2>
+      {
+        
+        course.map((course , index)=>{
+          <div key={course.id}>
+            <h1>{course.title}</h1>
+            <h2>{course.description}</h2>
+          </div>
+        })
+      }
     </>
   )
 }
