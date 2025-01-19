ef0919e0c9b884a7c9b6d00d3138838508b0020a
tejaxhvi <tajeshviyadav@gmail.com>
2025-01-19 12:13:01 +0530

    More Optimized Top Bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,39 +9,30 @@ import './App.css'
 
 function App() {
   const [course, setCourse] = useState([])
+  const navigate = useNavigate();
 
-  // useEffect(()=>{
-  //   axios.get('http://localhost:3001/api/courses')
-  //   .then((response)=>{
-  //     setCourse(response.data.Courses)
-  //     console.log(response.data.Courses);
-      
-  //   })
-  // },[])
+
   return (
     <>
-     <BrowserRouter>
+    <div style={{display: "flex",gap:"40vw",width:'full'}}>
+        <h1 style={{ color : 'red' , marginLeft : "150px" , fontSize : "50px",cursor : "pointer"}} onClick={()=> navigate('/')}>Course Selling Website</h1>
+        <div style={{margin:"40px",display: "flex",gap:"30px"}}>
+            <button style={{height : "40px" , width : "120px", }} onClick={()=> navigate('/signup')} > Sign-up</button>
+            <button style={{height : "40px" , width : "120px", }}  onClick={()=> navigate('/signup')} >Login</button>
+         </div>
+    </div>
       <Routes>
           <Route path="/signup" element={<Signup/>}></Route>
           <Route path="/login" element={<Login/>}></Route>
           <Route path="/" element={<Landing/>}></Route>
       </Routes>
-      </BrowserRouter>
     </>
   )
 }
 
 function Landing() {
-  const navigate = useNavigate();
   return <>
-  <div style={{display: "flex",gap:"40vw",width:'full'}}>
-    <h1 style={{ color : 'red' , marginLeft : "150px" , fontSize : "50px"}}>Course Selling Website</h1>
-    <div style={{margin:"40px",display: "flex",gap:"30px"}}>
-    <button style={{height : "40px" , width : "120px", }} onClick={()=> navigate('/signup')} > Sign-up</button>
-    <button style={{height : "40px" , width : "120px", }}  onClick={()=> navigate('/signup')} >Login</button>
-    </div>
-    </div>
-  
+   
   </>
 }
 
