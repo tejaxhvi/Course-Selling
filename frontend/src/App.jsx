6a7e0c31749e9e2ff7f10d8a43109d324767813b
tejaxhvi <tajeshviyadav@gmail.com>
2025-01-23 11:37:39 +0530

    Implemented the <Layout/> concept

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes , Route , useNavigate} from 'react-router-dom'
+import { Routes , Route , useNavigate , Outlet} from 'react-router-dom'
 
 import { Signup } from '../components/signup'
 import { Login } from '../components/login'
@@ -10,6 +10,19 @@ function App() {
 
   return (
     <>
+      <Routes>
+          <Route path="/" element={<Layout/>}>
+          <Route path="/signup" element={<Signup/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="*" element={<ErrorPage/>}></Route>
+          </Route>
+      </Routes>
+    </>
+  )
+}
+
+function Layout() {
+  return <div style={{height:"100vh" }}>
     <div style={{display: "flex",gap:"40vw",width:'full',height:"15vh",justifyContent: "space-between"}}>
         <h1 style={{ color : 'red' , margin : "30px" , fontSize : "40px",cursor : "pointer"}} onClick={()=> navigate('/')}>Course Selling </h1>
         <div style={{margin:"50px",display: "flex",gap:"30px",}}>
@@ -17,19 +30,17 @@ function App() {
             <button style={{height : "40px" , width : "120px", }}  onClick={()=> navigate('/login')} >Login</button>
          </div>
     </div>
-      <Routes>
-          <Route path="/signup" element={<Signup/>}></Route>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/" element={<Landing/>}></Route>
-      </Routes>
-    </>
-  )
+    <div style={{height:"100vh" , backgroundColor:"grey"}}><Outlet/></div>
+    <div style={{height:"10vh"}}><Footer/></div>
+  </div>
 }
-
-function Landing() {
-  return <>
-   
-  </>
+function Footer() {
+  return <div style={{backgroundColor:"red"}}>
+  <h1>This is Footer</h1>
+  </div>
+}
+function ErrorPage() {
+  
 }
 
 export default App
